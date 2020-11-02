6b16b5fb72677dd7fc2c45dadc66b6ce2c35db35
randnum <wiggins2000@gmail.com>
2020-11-02 16:44:17 +0100

    Added pagination functionality

--- GetReportingAAScript.rb
+++ GetReportingAAScript.rb
@@ -48,38 +48,56 @@ end
 #Create the report object
 report = Report.create!(name: 'new report')
 
+
+input_array = ARGV
+start_date = input_array[0]
+end_date = input_array[1]
+
+#puts "The Length of input_array is #{input_array.length}"
+
 # Create the connection and get the reporting data:
 url = URI("https://api.libring.com/v2/reporting/get\
-?period=custom_date&start_date=2020-03-10&end_date=2020-04-10&data_type=adnetwork,acquisition&allow_mock=true&group_by=app,platform,country,connection")
+?period=custom_date&start_date=#{start_date}&end_date=#{end_date}&data_type=adnetwork,acquisition&allow_mock=true&group_by=app,platform,country,connection")
 
 https = Net::HTTP.new(url.host, url.port);
 https.use_ssl = true
 
-request = Net::HTTP::Get.new(url)
-request["Authorization"] = "Token DHDwhdFXfoGBYLPOZPvTTwJoS"
 
-response = https.request(request)
-#puts response.read_body
-reportParsed = JSON.parse(response.read_body)
-connectionsParsed = reportParsed['connections']
-puts "The number of connections is #{connectionsParsed.size}"
+#handle pagination 
+loop do
+      request = Net::HTTP::Get.new(url)
+      request["Authorization"] = "Token DHDwhdFXfoGBYLPOZPvTTwJoS"
+      response = https.request(request)
+      #puts response.read_body
+      reportParsed = JSON.parse(response.read_body)
+      connectionsParsed = reportParsed['connections']
+      #puts "The number of connections is #{connectionsParsed.size}"
 
 
-# Create a few records...
-count = 0
-connectionsParsed.each do |i|
-    count+=1
-    report.connections.create!(
-        name: "connection #{count}",         #at least a simple iterator based on number of connections
-        connection: i["connection"],
-        app: i["app"],
-        country: i["country"],
-        date: i["date"],
-        impressions: i["impressions"],
-        ad_revenue: i["ad_revenue"]
-    )
-end
+      # Create all of the connection objects from this page...
+      count = 0
+      connectionsParsed.each do |i|
+          count+=1
+          report.connections.create!(
+              name: "connection #{count}",         #at least a simple iterator based on number of connections
+              connection: i["connection"],
+              app: i["app"],
+              country: i["country"],
+              date: i["date"],
+              impressions: i["impressions"],
+              ad_revenue: i["ad_revenue"]
+          )
+        end
+      puts "Added #{count} connections from this page"
+    if reportParsed["next_page_url"] == ""
+      break
+    end
+    puts "Going to next page url at #{reportParsed["next_page_url"]}" 
+    url = reportParsed["next_page_url"]
+  end
+
+
 
 connectionTypes = report.connections.pluck(:connection)
-puts "#{report.name} has #{report.connections.size} connections with connection types, #{connectionTypes.join(', ')}"
+#puts "#{report.name} has #{report.connections.size} connections with connection types, #{connectionTypes.join(', ')}"
 #output some test data 
